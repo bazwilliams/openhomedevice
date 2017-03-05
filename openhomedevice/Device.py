6ba7b7ec7c7436ab414dcb9adc056212a29fbbc6
bazwilliams <barry@bjw.me.uk>
2017-03-05 10:31:15 +0000

    Some Linting, Added Volume Enabled function and guard on volume info calls

--- openhomedevice/Device.py
+++ openhomedevice/Device.py
@@ -19,14 +19,14 @@ class Device(object):
         product = soapRequest(service.ControlUrl(), service.Type(), "Product", "")
 
         productXml = etree.fromstring(product)
-        return productXml[0].find("{urn:av-openhome-org:service:Product:2}ProductResponse/Name").text.encode('utf-8')
+        return productXml[0].find("{%s}ProductResponse/Name" % service.Type()).text.encode('utf-8')
 
     def Room(self):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Product")
         product = soapRequest(service.ControlUrl(), service.Type(), "Product", "")
 
         productXml = etree.fromstring(product)
-        return productXml[0].find("{urn:av-openhome-org:service:Product:2}ProductResponse/Room").text.encode('utf-8')
+        return productXml[0].find("{%s}ProductResponse/Room" % service.Type()).text.encode('utf-8')
 
     def SetStandby(self, standbyRequested):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Product")
@@ -43,40 +43,40 @@ class Device(object):
         standbyState = soapRequest(service.ControlUrl(), service.Type(), "Standby", "")
         
         standbyStateXml = etree.fromstring(standbyState)
-        return standbyStateXml[0].find("{urn:av-openhome-org:service:Product:2}StandbyResponse/Value").text == "true"
+        return standbyStateXml[0].find("{%s}StandbyResponse/Value" % service.Type()).text == "true"
 
     def TransportState(self):
         source = self.Source()
-        if (source["type"] == "Radio"):
+        if source["type"] == "Radio":
             return self.RadioTransportState()
-        if (source["type"] == "Playlist"):
+        if source["type"] == "Playlist":
             return self.PlaylistTransportState()
         return ""
 
     def Play(self):
         source = self.Source()
-        if (source["type"] == "Radio"):
+        if source["type"] == "Radio":
             return self.PlayRadio()
-        if (source["type"] == "Playlist"):
+        if source["type"] == "Playlist":
             return self.PlayPlaylist()
 
     def Stop(self):
         source = self.Source()
-        if (source["type"] == "Radio"):
+        if source["type"] == "Radio":
             return self.StopRadio()
-        if (source["type"] == "Playlist"):
+        if source["type"] == "Playlist":
             return self.StopPlaylist()
 
     def Pause(self):
         source = self.Source()
-        if (source["type"] == "Radio"):
+        if source["type"] == "Radio":
             return self.StopRadio()
-        if (source["type"] == "Playlist"):
+        if source["type"] == "Playlist":
             return self.PausePlaylist()
 
     def Skip(self, offset):
         source = self.Source()
-        if (source["type"] == "Playlist"):
+        if source["type"] == "Playlist":
             service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Playlist")
 
             command = None
@@ -93,14 +93,14 @@ class Device(object):
         transportState = soapRequest(service.ControlUrl(), service.Type(), "TransportState", "")
 
         transportStateXml = etree.fromstring(transportState)
-        return transportStateXml[0].find("{urn:av-openhome-org:service:Radio:1}TransportStateResponse/Value").text
+        return transportStateXml[0].find("{%s}TransportStateResponse/Value" % service.Type()).text
 
     def PlaylistTransportState(self):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Playlist")
         transportState = soapRequest(service.ControlUrl(), service.Type(), "TransportState", "")
 
         transportStateXml = etree.fromstring(transportState)
-        return transportStateXml[0].find("{urn:av-openhome-org:service:Playlist:1}TransportStateResponse/Value").text
+        return transportStateXml[0].find("{%s}TransportStateResponse/Value" % service.Type()).text
 
     def PlayRadio(self):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Radio")
@@ -127,25 +127,44 @@ class Device(object):
         source = soapRequest(service.ControlUrl(), service.Type(), "SourceIndex", "")
 
         sourceXml = etree.fromstring(source)
-        sourceIndex = sourceXml[0].find("{urn:av-openhome-org:service:Product:2}SourceIndexResponse/Value").text
+        sourceIndex = sourceXml[0].find("{%s}SourceIndexResponse/Value" % service.Type()).text
 
         sourceInfo = soapRequest(service.ControlUrl(), service.Type(), "Source", ("<Index>%s</Index>" % int(sourceIndex)))
         sourceInfoXml = etree.fromstring(sourceInfo)
 
-        sourceName = sourceInfoXml[0].find("{urn:av-openhome-org:service:Product:2}SourceResponse/Name").text
-        sourceType = sourceInfoXml[0].find("{urn:av-openhome-org:service:Product:2}SourceResponse/Type").text
+        sourceName = sourceInfoXml[0].find("{%s}SourceResponse/Name" % service.Type()).text
+        sourceType = sourceInfoXml[0].find("{%s}SourceResponse/Type" % service.Type()).text
         
         return {
             "type": sourceType,
             "name": sourceName
         }
 
+    def VolumeEnabled(self):
+        service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
+        return service != None
+
     def VolumeLevel(self):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
+
+        if service is None:
+            return None
+
         volume = soapRequest(service.ControlUrl(), service.Type(), "Volume", "")
 
         volumeXml = etree.fromstring(volume)
-        return int(volumeXml[0].find("{urn:av-openhome-org:service:Volume:2}VolumeResponse/Value").text)
+        return int(volumeXml[0].find("{%s}VolumeResponse/Value" % service.Type()).text)
+
+    def IsMuted(self):
+        service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
+
+        if service is None:
+            return None
+
+        mute = soapRequest(service.ControlUrl(), service.Type(), "Mute", "")
+
+        muteXml = etree.fromstring(mute)
+        return muteXml[0].find("{%s}MuteResponse/Value" % service.Type()).text == "true"
 
     def SetVolumeLevel(self, volumeLevel):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
@@ -159,13 +178,6 @@ class Device(object):
     def DecreaseVolume(self):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
         soapRequest(service.ControlUrl(), service.Type(), "VolumeDec", "")
-
-    def IsMuted(self):
-        service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
-        mute = soapRequest(service.ControlUrl(), service.Type(), "Mute", "")
-
-        muteXml = etree.fromstring(mute)
-        return muteXml[0].find("{urn:av-openhome-org:service:Volume:2}MuteResponse/Value").text == "true"
 
     def SetMute(self, muteRequested):
         service = self.rootDevice.Device().Service("urn:av-openhome-org:serviceId:Volume")
@@ -186,7 +198,7 @@ class Device(object):
         sources = soapRequest(service.ControlUrl(), service.Type(), "SourceXml", "")
 
         sourcesXml = etree.fromstring(sources)
-        sourcesList = sourcesXml[0].find("{urn:av-openhome-org:service:Product:2}SourceXmlResponse/Value").text
+        sourcesList = sourcesXml[0].find("{%s}SourceXmlResponse/Value" % service.Type()).text
 
         sourcesListXml = etree.fromstring(sourcesList)
 
@@ -194,7 +206,7 @@ class Device(object):
         index = 0
         for sourceXml in sourcesListXml:
             visible = sourceXml.find("Visible").text == "true"
-            if (visible):
+            if visible:
                 sources.append({
                     "index": index,
                     "name": sourceXml.find("Name").text,
@@ -208,9 +220,9 @@ class Device(object):
         trackInfo = soapRequest(service.ControlUrl(), service.Type(), "Track", "")
         
         trackInfoXml = etree.fromstring(trackInfo)
-        metadata = trackInfoXml[0][0].find('Metadata').text
+        metadata = trackInfoXml[0][0].find("Metadata").text
 
-        if (metadata == None):
+        if metadata is None:
             return {}
 
         metadataXml = etree.fromstring(metadata)
