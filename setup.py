2133ccb14ded40cd4691a744dd5260d7eea3ade2
bazwilliams <barry@bjw.me.uk>
2017-05-09 18:22:35 +0100

    Prepare for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'openhomedevice',
   packages = ['openhomedevice'],
-  version = '0.2.2',
+  version = '0.3.0',
   description = 'Provides an API for requesting information from an Openhome device',
   author = 'Barry John Williams',
   author_email = 'barry@bjw.me.uk',
